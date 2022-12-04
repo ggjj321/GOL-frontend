d8bcb97ba477d76a52d5786d97922e16b2894911
ggjj321 <10370324lsjh@gmail.com>
2022-12-04 23:31:05 +0800

    fix:sign up post request

--- gol-frontend/login-page/main.js
+++ gol-frontend/login-page/main.js
@@ -1,1 +1,26 @@
 document.documentElement.setAttribute('data-theme','dark');
+
+function signUpToCreateNewAccount(){
+    let name = $("#name").val();
+    let phone = $("#phone").val();
+    let email = $("#email").val();
+    let password = $("#password").val();
+    let comfirmPassword = $("#comfirmPassword").val();
+
+    if (password == comfirmPassword){
+        let user = JSON.stringify(
+            {
+                "name" : name,
+                "password" : password,
+                "phone" : phone,
+                "email" : email
+            }
+        );
+
+        console.log(typeof user);
+
+        $.post("http://127.0.0.1:8000/sign_up", {'user': user}, function(result){
+            console.log(user);
+        });
+    }
+}
