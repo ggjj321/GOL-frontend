8923266dbff924823fa336237eb7d34a8446c4be
Chung <kevin506059@gmail.com>
2022-12-26 03:51:59 +0800

    feat: starting up cookie

--- gol-frontend/login-page/main.js
+++ gol-frontend/login-page/main.js
@@ -1,1 +1,25 @@
 document.documentElement.setAttribute('data-theme','dark');
+
+function parseCookie() {
+    var cookieObj = {};
+    var cookieAry = document.cookie.split(';');
+    var cookie;
+    
+    for (var i=0, l=cookieAry.length; i<l; ++i) {
+        cookie = jQuery.trim(cookieAry[i]);
+        cookie = cookie.split('=');
+        cookieObj[cookie[0]] = cookie[1];
+    }
+    
+    return cookieObj;
+}
+
+
+function getCookieByName(name) {
+    var value = parseCookie()[name];
+    if (value) {
+        value = decodeURIComponent(value);
+    }
+
+    return value;
+}
