855737d2577482aece2e3967c8ae90ec64ac7482
ggjj321 <10370324lsjh@gmail.com>
2022-12-30 00:20:46 +0800

    fix login 422

--- gol-frontend/login-page/main.js
+++ gol-frontend/login-page/main.js
@@ -1,29 +1,33 @@
-document.documentElement.setAttribute('data-theme','dark');
+document.documentElement.setAttribute('data-theme', 'dark');
 
 let tokenCookie = document.cookie;
 
-function signUpToCreateNewAccount(){
+function signUpToCreateNewAccount() {
     let name = $("#name").val();
     let phone = $("#phone").val();
     let email = $("#email").val();
     let password = $("#password").val();
     let comfirmPassword = $("#comfirmPassword").val();
 
-    if (password == comfirmPassword){
+    if (password == comfirmPassword) {
         let account = {
-            'name' : name,
-            'password' : password,
-            'phone' : phone,
-            'email' : email
+            'username': name,
+            'password': password,
+            'phone': phone,
+            'email': email
         };
 
         $.ajax({
-            type : "POST",
-            url : "http://127.0.0.1:8000/sign_up",
-            data : JSON.stringify(account),
-            contentType : "application/json",
-            success : (response) => console.log(response),
-        })
+            type: "POST",
+            url: "http://127.0.0.1:80/sign_up",
+            data: account,
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            success: (response) => console.log(response),
+            error: (response) => console.log(response),
+        });
     }
 }
 
@@ -33,28 +37,21 @@ const createCookie = (userInfo) => {
 
 
 const onclickLogin = () => {
-    // let formData = new FormData();
-    // formData.append('username', 'kll');
-    // formData.append('password', '123456');
-    // console.log(formData);
+    let userInfo = {
+        "username": document.getElementById("username").value,
+        "password": document.getElementById("password").value
+    };
 
-    const userInfo = {
-        'username' : document.getElementById("username").value,
-        'password' : document.getElementById("password").value
-    }
-    console.log(userInfo);
-
-    fetch('http://0.0.0.0:80/login', {
-        method: 'POST',
-        body: JSON.stringify(userInfo),
-    })
-    .then(response => {
-        console.log(response);
-        createCookie(userInfo);
-    })
-    .catch(err => {
-        console.log(err)
-    })
-    console.log(tokenCookie);
+    $.ajax({
+        type: "POST",
+        url: "http://127.0.0.1:80/login",
+        data: userInfo,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        success: (response) => console.log(response),
+        error: (response) => console.log(response),
+    });
 };
 
